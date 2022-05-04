c29c68e0c49a72c474bb133476fbb10fb748d86b
charlotte-wt <charlottewee01@gmail.com>
2022-05-04 17:20:11 +0800

    edited pom.xml and tested that the app runs

--- gto/src/main/java/com/example/gto/GtoApplication.java
+++ gto/src/main/java/com/example/gto/GtoApplication.java
@@ -2,6 +2,8 @@ package com.example.gto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GtoApplication {
@@ -9,5 +11,17 @@ public class GtoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GtoApplication.class, args);
 	}
+
+	
+
+}
+
+@RestController
+class HelloControler {
+
+  @RequestMapping("/")
+  public String hello() {
+    return "Hello Spring Booot";
+  }
 
 }
