67d22603cd3126980b834ba4e7d980b1d033c3a8
charlotte-wt <charlottewee01@gmail.com>
2022-05-04 16:40:30 +0800

    Added project from spring initializr

---
+++ gto/src/main/java/com/example/gto/GtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtoApplication.class, args);
+	}
+
+}
---
+++ gto/src/test/java/com/example/gto/GtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
