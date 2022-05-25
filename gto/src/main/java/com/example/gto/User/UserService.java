6a8cfd4266799f190288cf3ba608f8b4e9f971d1
charlotte-wt <charlottewee01@gmail.com>
2022-05-25 09:03:24 +0800

    Commented out UserService

---
+++ gto/src/main/java/com/example/gto/User/UserService.java
@@ -0,0 +1,22 @@
+//package com.example.gto.User;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public interface UserService {
+//    List<User> listUser();
+//    User getUser(String email);
+//    User addUser(User User);
+//    User updateUser(String email, User User);
+//
+//
+//    /**
+//     * Change method's signature: do not return a value for delete operation
+//     * @param id
+//     */
+//
+//
+//    void deleteUser(Long id);
+//}
