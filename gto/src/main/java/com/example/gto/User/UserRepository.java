b26b649b319bd5ef40bbe1d734708a054b74cae6
charlotte-wt <charlottewee01@gmail.com>
2022-05-31 19:42:35 +0800

    Update UserRepository.java

--- gto/src/main/java/com/example/gto/User/UserRepository.java
+++ gto/src/main/java/com/example/gto/User/UserRepository.java
@@ -3,11 +3,8 @@ package com.example.gto.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends MongoRepository<User, String>  {
     public User findByEmail(String email);
-    public List<User> findByUserName(String userName);
 
 }
