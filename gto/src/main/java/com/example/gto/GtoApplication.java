5223dff6074e35bf9e4fb4af1e9182e0b5147f3b
charlotte-wt <charlottewee01@gmail.com>
2022-06-03 01:45:46 +0800

    Update GtoApplication.java

--- gto/src/main/java/com/example/gto/GtoApplication.java
+++ gto/src/main/java/com/example/gto/GtoApplication.java
@@ -1,7 +1,11 @@
 package com.example.gto;
 
+import com.example.gto.User.Role;
+import com.example.gto.User.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
@@ -12,7 +16,28 @@ public class GtoApplication {
 		SpringApplication.run(GtoApplication.class, args);
 	}
 
-	
+	// Test
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+
+		return args -> {
+
+			Role adminRole = roleRepository.findByRole("ADMIN");
+			if (adminRole == null) {
+				Role newAdminRole = new Role();
+				newAdminRole.setRole("ADMIN");
+				roleRepository.save(newAdminRole);
+			}
+
+			Role userRole = roleRepository.findByRole("USER");
+			if (userRole == null) {
+				Role newUserRole = new Role();
+				newUserRole.setRole("USER");
+				roleRepository.save(newUserRole);
+			}
+		};
+
+	}
 
 }
 
