8328bbfd54e35c4e0f9c6cb9119ffb80cab9801e
charlotte-wt <charlottewee01@gmail.com>
2022-06-25 02:27:43 +0800

    changed eventController to return response entity with http status

--- gto/src/main/java/com/example/gto/Events/EventController.java
+++ gto/src/main/java/com/example/gto/Events/EventController.java
@@ -1,10 +1,13 @@
 package com.example.gto.Events;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,35 +22,95 @@ public class EventController {
 
     @GetMapping("")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    public List<Event> getEvents() {
-        return eventRepository.findAll();
+    public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false) String title) {
+        try {
+            List<Event> events = new ArrayList<Event>();
+
+            if (title == null)
+                eventRepository.findAll().forEach(events::add);
+            else
+                eventRepository.findByTitleContaining(title).forEach(events::add);
+
+            if (events.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(events, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("findEventById/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable("id") String id) {
+        Optional<Event> eventData = eventRepository.findById(id);
+
+        if (eventData.isPresent()) {
+            return new ResponseEntity<>(eventData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
 
     @GetMapping("findEventByTitle/{title}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    public Optional<Event> getEventByTitle(@PathVariable String title) {
-        return eventRepository.findByTitle(title);
+    public ResponseEntity<Event> getEventByTitle(@PathVariable String title) {
+        Optional<Event> eventData = eventRepository.findByTitle(title);
+        if (eventData.isPresent()) {
+            return new ResponseEntity<>(eventData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("findEventByUsername/{username}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    public Optional<Event> getFriendByUsername(@PathVariable String username) {
-        return eventRepository.findByUsername(username);
+    public ResponseEntity<Event> getFriendByUsername(@PathVariable String username) {
+        Optional<Event> eventData = eventRepository.findByUsername(username);
+        if (eventData.isPresent()) {
+            return new ResponseEntity<>(eventData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("addEvent")
+    @PostMapping("")
     @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
-    public String addEvent(@Valid @RequestBody Event event) {
-        eventRepository.save(event);
-        return "Event " + event.getDescription() + " has been added by " + event.getUsername();
+    public ResponseEntity<Event> addEvent(@Valid @RequestBody Event event) {
+        try {
+            Event _event = eventRepository.save(event);
+            return new ResponseEntity<>(_event, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+    public ResponseEntity<Event> updateEvent(@PathVariable("id") String id, @RequestBody Event event) {
+        Optional<Event> eventData = eventRepository.findById(id);
+
+        if (eventData.isPresent()) {
+            Event _event = eventData.get();
+            _event.setUsername(event.getUsername());
+            _event.setUsername(event.getEmail());
+            _event.setTitle(event.getTitle());
+            _event.setDescription(event.getDescription());
+            return new ResponseEntity<>(eventRepository.save(_event), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
     @DeleteMapping("delete/{title}")
     @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
-    public String deleteEvent(@PathVariable Event event) {
-        eventRepository.delete(event);
-        return "Event " + event.getTitle() + " deleted by: " +  event.getUsername();
+    public ResponseEntity<Event> deleteEvent(@PathVariable Event event) {
+        try {
+            eventRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
